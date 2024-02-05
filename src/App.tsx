08795d2e521153fa4088e757aa094c6fbd9a1240
OgaDavid <oluwademiladedavidoga@gmail.com>
2024-02-05 23:11:17 +0100

    Revert "add inter font from google fonts"
    This reverts commit 89d31b21e8580db8890ca524cc7b4a7f64aedd2a.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
-import RatesComponent from "@/components/rates/rates-component";
-
 function App() {
   return (
-    <div className="py-14">
-      <div className="container">
-        <h1 className="text-[40px] satoshi text-custom-black font-medium">
-          Special Rates
-        </h1>
-        <div>
-          <RatesComponent />
-        </div>
+    <>
+      <div>
+        <h1>App</h1>
       </div>
-    </div>
+    </>
   );
 }
 
