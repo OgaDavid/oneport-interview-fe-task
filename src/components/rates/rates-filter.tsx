cd1944c3215b03770207e010e3b2511fddd3be1a
OgaDavid <oluwademiladedavidoga@gmail.com>
2024-02-06 11:20:30 +0100

    add rate filter component

---
+++ src/components/rates/rates-filter.tsx
@@ -0,0 +1,29 @@
+const filter = [
+  "COSCO",
+  "CMA CGM",
+  "MAERSK",
+  "PIL",
+  "ZIM",
+  "OOCL",
+  "MSC",
+  "ONE",
+  "ESL",
+  "OVERGREEN",
+];
+
+const RatesFilter = () => {
+  return (
+    <div className="flex scrollbar items-center gap-x-3 max-w-[520px] lg:max-w-[750px] overflow-auto">
+      {filter.map((filter, i) => (
+        <div
+          key={i}
+          className="flex items-center gap-x-2 px-4 py-3 border-solid border-[1px] rounded w-auto min-w-fit cursor-pointer border-[#9CA3AF] text-[#1F2937]"
+        >
+          {filter}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RatesFilter;
--- src/components/rates/rates-header.tsx
+++ src/components/rates/rates-header.tsx
@@ -1,12 +1,14 @@
+import RatesFilter from "./rates-filter";
 import { RateSelect } from "./rates-select";
 
 const RatesHeader = () => {
   return (
-    <div className="border-b pb-8 border-custom-border-grey">
+    <div className="border-b max-md:flex-col max-md:justify-start max-md:items-start gap-x-3 gap-y-5 flex items-center justify-between pb-8 border-custom-border-grey">
       <div className="flex items-center gap-x-3">
         <RateSelect type="size" />
         <RateSelect type="type" />
       </div>
+      <RatesFilter />
     </div>
   );
 };
