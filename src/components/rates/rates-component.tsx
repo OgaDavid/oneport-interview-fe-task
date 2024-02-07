9bc3ddc857f74af8567e92139642b4b21c9bf730
OgaDavid <oluwademiladedavidoga@gmail.com>
2024-02-07 16:17:49 +0100

    add rates oagination

--- src/components/rates/rates-component.tsx
+++ src/components/rates/rates-component.tsx
@@ -1,14 +1,15 @@
+import axios from "axios";
 import RatesHeader from "@/components/rates/rates-header";
 import { useRatesParamsStore } from "@/store/rates-params-store";
 import { useEffect, useState } from "react";
 import { useRatesStore } from "@/store/rates-store";
-import axios from "axios";
 import Loading from "@/components/ui/loading";
 import RateCard from "./rate-card";
 import RateContainer from "./rate-container";
 import { getRateFilter } from "@/action/get-rate-filters";
 
 const RatesComponent = () => {
+  const [noOfRates, setNoOfRates] = useState(9);
   const rateStore = useRatesStore((state) => state.rates);
   const rateFilters = useRatesParamsStore((state) => state.rateFilters);
   const setRatesStore = useRatesStore((state) => state.setRates);
@@ -38,7 +39,7 @@ const RatesComponent = () => {
           setRateFilters(rateFilters);
         });
       } catch (error) {
-        console.log(error);
+        console.error(error);
       } finally {
         setIsLoading(false);
       }
@@ -47,17 +48,19 @@ const RatesComponent = () => {
     fetchRates();
   }, [API_URL]);
 
+  const paginatedRates = rateStore.slice(0, noOfRates);
+
   return (
     <>
+      <RatesHeader rateFilters={rateFilters} />
       {isLoading ? (
         <div className="flex items-center justify-center">
           <Loading />
         </div>
       ) : (
         <div>
-          <RatesHeader rateFilters={rateFilters} />
           <RateContainer>
-            {rateStore.map((rate, i) => (
+            {paginatedRates.map((rate, i) => (
               <RateCard
                 key={i}
                 amountUsd={rate.total_amount_usd}
@@ -71,6 +74,31 @@ const RatesComponent = () => {
               />
             ))}
           </RateContainer>
+          <div className="mt-10">
+            <p className="text-center mb-4 text-sm text-custom-black">
+              Viewing {paginatedRates.length} of {rateStore.length} special
+              rates
+            </p>
+            {paginatedRates.length <= 9 ? (
+              <button
+                className="border-solid flex px-12 mx-auto border-[1px] border-[#374151] rounded py-3"
+                onClick={() => {
+                  setNoOfRates(rateStore.length);
+                }}
+              >
+                Show All
+              </button>
+            ) : (
+              <button
+                className="border-solid flex px-12 mx-auto border-[1px] border-[#374151] rounded py-3"
+                onClick={() => {
+                  setNoOfRates(9);
+                }}
+              >
+                Show Less
+              </button>
+            )}
+          </div>
         </div>
       )}
     </>
--- src/components/ui/loading.tsx
+++ src/components/ui/loading.tsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className="border-custom-grey flex items-center justify-center h-8 w-8 animate-spin rounded-full border-[3px] border-t-custom-green" />
+    <div className="border-custom-grey mt-10 flex items-center justify-center h-8 w-8 animate-spin rounded-full border-[3px] border-t-custom-green" />
   );
 };
 
