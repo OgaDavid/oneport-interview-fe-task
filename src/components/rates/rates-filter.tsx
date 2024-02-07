5853d3851318fd9d187c7e0a63c9bb9571f72738
OgaDavid <oluwademiladedavidoga@gmail.com>
2024-02-07 06:48:11 +0100

    add rate filtering

--- src/components/rates/rates-filter.tsx
+++ src/components/rates/rates-filter.tsx
@@ -1,21 +1,19 @@
-// const filter = [
-//   "COSCO",
-//   "CMA CGM",
-//   "MAERSK",
-//   "PIL",
-//   "ZIM",
-//   "OOCL",
-//   "MSC",
-//   "ONE",
-//   "ESL",
-//   "OVERGREEN",
-// ];
+import { useRatesStore } from "@/store/rates-store";
 
 const RatesFilter = ({ filters }: { filters: string[] }) => {
+  const rateStore = useRatesStore((state) => state.rates);
+  const setRates = useRatesStore((state) => state.setRates);
   return (
     <div className="flex scrollbar items-center gap-x-3 max-w-[520px] lg:max-w-[750px] overflow-auto">
       {filters.map((filter, i) => (
         <div
+          onClick={() => {
+            setRates(
+              rateStore.filter((rate) => {
+                return rate.carrier_name === filter;
+              })
+            );
+          }}
           key={i}
           className="flex items-center gap-x-2 px-4 py-3 border-solid border-[1px] rounded w-auto min-w-fit cursor-pointer border-[#9CA3AF] text-[#1F2937]"
         >
